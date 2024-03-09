35048c17ecb81056d237318f451751cecac05a73
bhaves19h <162792561+bhaves19h@users.noreply.github.com>
2024-03-09 16:39:20 +0530

    Add files via upload

---
+++ game1.js
@@ -0,0 +1,9 @@
+let num=18;
+let usernum = prompt("Guess the number");
+
+while(usernum !=num){
+    usernum =prompt("Wrong number Guess again");
+};
+if (usernum==num){
+console.log("Congratulatons");
+};
